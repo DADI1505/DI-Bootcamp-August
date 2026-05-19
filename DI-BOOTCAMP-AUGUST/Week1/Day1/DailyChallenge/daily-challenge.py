e6d3323832c78397d40845a6e5713554cbcf01e3
DADI1505 <dadichris08@gmail.com>
2026-05-19 14:20:39 +0000

    challenge-daily-1&2

--- DI-BOOTCAMP-AUGUST/Week1/Day1/DailyChallenge/daily-challenge.py
+++ DI-BOOTCAMP-AUGUST/Week1/Day1/DailyChallenge/daily-challenge.py
@@ -1,19 +1,23 @@
-# """
-# Challenge 1
-# """
-# #ask number and length to user 
-# number = int(input("Enter number : "))
-# length = int(input("Enter length : "))
-# mutiple =[]
-# for i in range(1,length+1) :
-#     mutiple.append(number*i)
+"""
+Challenge 1
+"""
+#ask number and length to user 
+number = int(input("Enter number : "))
+length = int(input("Enter length : "))
+mutiple =[]
+for i in range(1,length+1) :
+    mutiple.append(number*i)
 
-# print(mutiple)
+print(mutiple)
 
 """
 Challenge 2
 """
 #ask string to user 
 user = input("Enter a string : ")
-for i in user :
-    i
+for i1 in user :
+    a = i1
+    for i2 in user :
+        if i2 == a and user.count(a)!=1 :
+            user = user.replace(a,"",1)
+print(user)
