4cc0dcd051a7bf640babf7f581a0fbd364e5291b
DADI1505 <dadichris08@gmail.com>
2026-05-19 11:05:30 +0000

    first commit

---
+++ DI-BOOTCAMP-AUGUST/Week1/Day1/ExerciseXP/exercise-xp.py
@@ -0,0 +1,25 @@
+"""
+EXERCISE 1 : HELLO WORLD
+print hello word
+"""
+print("hello world")
+print("hello world")
+print("hello world")
+print("hello world")
+
+
+"""
+EXERCISE 2 : SOME MATH
+Write code that calculates the result of:
+"""
+print( (99**3)*8)
+
+"""
+EXERCISE 3 : What is the output?
+
+"""
+
+
+
+
+
