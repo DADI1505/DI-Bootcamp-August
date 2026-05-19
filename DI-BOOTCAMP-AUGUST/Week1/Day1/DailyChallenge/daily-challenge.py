58a9a448e92fa08f8c452b5474c190adf59b4d55
DADI1505 <dadichris08@gmail.com>
2026-05-19 15:37:59 +0000

    correct daily-challenge1&2

--- DI-BOOTCAMP-AUGUST/Week1/Day1/DailyChallenge/daily-challenge.py
+++ DI-BOOTCAMP-AUGUST/Week1/Day1/DailyChallenge/daily-challenge.py
@@ -8,16 +8,19 @@ mutiple =[]
 for i in range(1,length+1) :
     mutiple.append(number*i)
 
-print(mutiple)
+print(multiple)
 
 """
 Challenge 2
 """
 #ask string to user 
 user = input("Enter a string : ")
-for i1 in user :
-    a = i1
-    for i2 in user :
-        if i2 == a and user.count(a)!=1 :
-            user = user.replace(a,"",1)
+result = ""
+for char in user :
+    prev_char = char
+    for current_char in user :
+        if current_char == prev_char and result.count(prev_char) <1  :
+            result += prev_char
+            
+user = result  
 print(user)
