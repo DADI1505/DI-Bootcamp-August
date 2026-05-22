32dc3a446e6a8dc096c2019179ca67ec4f4c5021
DADI1505 <dadichris08@gmail.com>
2026-05-22 12:05:11 +0000

    dailychallenge-day4

--- DI-BOOTCAMP-AUGUST/Week1/Day4/DailyChallenge/daily-challenge.py
+++ DI-BOOTCAMP-AUGUST/Week1/Day4/DailyChallenge/daily-challenge.py
@@ -1,19 +1,64 @@
 import math
 
 class Pagination :
-    current_idx = None
     def __init__(self,items=None,page_size=10):
-        if items == None :
-            self.items = []
-            
+        self.items = items if items is not None else []  # ✅ on sauvegarde items
+        self.current_idx =0   
         self.page_size = page_size
-        Pagination.current_idx = self 
+        self.total_page = math.ceil (len(self.items)/self.page_size)
+        
          
-    def get_visible_items(cls):
-        return cls.current_idx.items
+    def get_visible_items(self):
+        start = self.current_idx * self.page_size
+        end = self.current_idx + self.page_size
+        return self.items[start:end]
+           
+    def go_to_page(self,page_num) : 
+         if not page_num in range(0,self.total_page+1) :
+             raise ValueError("Value isn't in pagination")
+         self.current_idx = page_num
+         
+    def first_page(self):
+        self.go_to_page(0)
+        
+    def last_page(self):
+        self.go_to_page(self.total_page)
+        
+    def next_page(self):
+        self.go_to_page(self.current_idx+1)
+        
+    def previous_page(self):
+        self.go_to_page(self.current_idx-1)
+    def __str__(self) :
+       return "\n".join(self.get_visible_items())
+
+
+
+
+alphabetList = list("abcdefghijklmnopqrstuvwxyz")
+p = Pagination(alphabetList, 4)
+
+print(p.get_visible_items())
+# ['a', 'b', 'c', 'd']
+
+p.next_page()
+print(p.get_visible_items())
+# ['e', 'f', 'g', 'h']
+
+p.last_page()
+print(p.get_visible_items())
+# ['y', 'z']
+
+p.go_to_page(0)
+# Raises ValueError
+
+p.go_to_page(10)
+print(p.current_idx + 1)
+# Output: ValueError
+
+
     
-    def go_to_page(cls,page_num) : 
-        cls.current_idx = page_num
+    
     
         
         
---
+++ DI-BOOTCAMP-AUGUST/Week1/Day4/DailyChallenge/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+p.go_to_page(10)
+print(p.current_idx + 1)
+# Output: ValueError
